1a19b545dcb76d74ca35d8906942938695fa2802
impe <romain.lucas@epitech.eu>
2025-07-08 15:39:34 +0200

    repeater ex with benchmark and strings.Builder

---
+++ iteration/repeat.go
@@ -0,0 +1,12 @@
+package iteration
+
+import "strings"
+
+func Repeat(character string) string {
+	var repeated strings.Builder
+
+	for range 5 {
+		repeated.WriteString(character)
+	}
+	return repeated.String()
+}
