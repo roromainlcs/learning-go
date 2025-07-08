b602971f081e763d00c2c3bd2e66d2c139c36c16
impe <romain.lucas@epitech.eu>
2025-07-08 12:53:46 +0200

    hello name + test

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Worldd"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper() // tells the test suit that this method is a helper (The failed tests will report a test fail to the line calling this method)
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
